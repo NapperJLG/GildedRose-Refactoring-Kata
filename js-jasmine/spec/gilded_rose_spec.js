5e87fa8b11626c5929c1a65c5455c516bb87e47b
Jake Napper <napperjlg@gmail.com>
2019-06-06 14:13:36 +0100

    create tests for special items

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -32,13 +32,50 @@ describe("Gilded Rose", function() {
     expect(items[0].quality).toEqual(0);
   });
 
-
-
-  describe('special rules', function(){
-
-
-
+  it('increases the quality of Aged Brie the older it gets', function(){
+    const gildedRose = new Shop([ new Item("Aged Brie", 10, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(11);
   });
+
+  it('can never make the quality of an item more than 50', function(){
+    const gildedRose = new Shop([ new Item("Aged Brie", 10, 50) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it('keeps the quality and SellIn of Sulfuras the same', function(){
+    const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 10, 50) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(10);
+    expect(items[0].quality).toEqual(50);
+  });
+
+  it('increases the quality of backstage passes by 1 when SellIn is more than 10', function(){
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 12, 30) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(31);
+  });
+
+  it('increases the quality of backstage passes by 2 when SellIn is less than 10', function() {
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(32);
+  });
+
+  it('increases the quality of backstage passes by 3 when SellIn is less than 5', function() {
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(33);
+  });
+
+  it('reduces the quality of backstage passes to 0 when SellIn is less than 0', function() {
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 30) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(0);
+  });
+
+
 
 
 
