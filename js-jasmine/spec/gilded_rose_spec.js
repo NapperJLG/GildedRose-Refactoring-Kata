52b9ea6e9e830edad1a323179fd69122791e9a4c
Jake Napper <napperjlg@gmail.com>
2019-06-06 11:40:38 +0100

    tests written for basic update quality requirements

--- js-jasmine/spec/gilded_rose_spec.js
+++ js-jasmine/spec/gilded_rose_spec.js
@@ -7,4 +7,18 @@ describe("Gilded Rose", function() {
     expect(items[0].name).toEqual('foo');
   });
 
+  it('should lower the sellIn value by one', function() {
+    const gildedRose = new Shop([ new Item("foo", 10, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).toEqual(9);
+  });
+
+  it('should lower the quality value by one', function() {
+    const gildedRose = new Shop([ new Item("foo", 10, 10) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toEqual(9);
+  });
+
+
+
 });
